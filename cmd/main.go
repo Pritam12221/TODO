da8a9c26783f46e3500c59c8a701d3e6e71566b6
Pritam12221 <pritam.satsangi@remotestate.in>
2026-04-21 16:46:08 +0530

    added search and update functionality

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	db "TODO/database"
+	s "TODO/server"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Println(".env file not found")
+	}
+
+	
+	log.Println("host check", os.Getenv("DB_HOST"))
+	log.Println("port check", os.Getenv("DB_PORT"))
+
+	err = db.ConnectAndMigrate(
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		db.SSLMode(os.Getenv("DB_SSLMODE")),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	srv:=s.ServerRoutes();
+	srv.Run();
+}
